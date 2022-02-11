ce4fbe025526d54505fb13624c6bf0b4a9d47676
Aaron Reason <aaronreason@hotmail.co.uk>
2022-02-11 17:37:04 +0000

    Finished Occupancy

--- src/Final/Final.ino
+++ src/Final/Final.ino
@@ -1,36 +1,13 @@
-byte IRR1 = 3;
-byte IRR2 = 4;
+#include "occupancy.h"
 
-byte IRL1 = 6;
-byte IRL2 = 7;
+const byte LEDLR = 13;
+const byte LEDLG = 12;
 
-byte LEDLR = 13;
-byte LEDLG = 12;
+const byte LEDRR = 11;
+const byte LEDRG = 10;
 
-byte LEDRR = 11;
-byte LEDRG = 10;
-
-//byte LIR = [IRL1, IRL2];
-//byte RIR = [IRR1, IRR2];
-
-enum Section 
-{
-  A,
-  B,
-  C
-};
-
-enum Destination
-{
-  Salisbury,
-  Wilton
-};
-
-byte IRArray[4] = {IRL1, IRL2, IRR1, IRR2};
-byte tempValue = 0;
-
-Section section = C;
-Destination destination = Salisbury;
+Section section = A;
+Destination destination = Wilton;
 
 
 void setup() {
@@ -46,38 +23,10 @@ void setup() {
   pinMode(LEDRG, OUTPUT);
 
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
 
-  for(int i = 0; i < sizeof(IRArray)/sizeof(byte); i++)
-  {
-    if(digitalRead(IRArray[i]) == 0) 
-    {
-      tempValue = IRArray[i];
-    }
-  }
-
-  if(tempValue == IRL1) 
-  {
-    if(destination == Salisbury) return;
-    section = B;
-  }
-  else if(tempValue == IRL2)
-  {
-    if(destination == Wilton) return;
-    section = A;
-  }
-  else if(tempValue == IRR1)
-  {
-    if(destination == Salisbury) return;
-    section = C;
-  }
-  else if(tempValue == IRR2)
-  {
-    if(destination == Wilton) return;
-    section = B;
-  }
+  setNextSection(section);
   
   Serial.println(section);
 }
---
+++ src/Final/occupancy.h
@@ -0,0 +1,107 @@
+#pragma once
+
+
+const byte IRR1 = 3;
+const byte IRR2 = 4;
+
+const byte IRL1 = 6;
+const byte IRL2 = 7;
+
+enum Section 
+{
+  A = 0,
+  B,
+  C
+};
+
+enum Destination
+{
+  Salisbury,
+  Wilton
+};
+
+/*
+    Array which contains the IR Sensors pins in
+    Salisbury -> Wilton Order
+    As we are currently only working with 3 section
+    and 2 IR Points we label them Left and Right
+    Left is Wilton, Right is Salisbury
+*/
+byte IRArray[4] = {IRL1, IRL2, IRR1, IRR2};
+
+// temporary value to calculate next section
+byte tempValue = 0;
+
+
+/*
+    MapData for each IR senser and their respective destination
+    Calculates the new section based on the destination and
+    index in the IRArray
+*/
+struct MapData
+{
+  byte pin;
+  Destination destination;
+  Section newSection;
+
+  MapData(byte pin, Destination destination):
+  pin(pin), destination(destination)
+  {
+    for(int i = 0; i < sizeof(IRArray) / sizeof(byte); i++)
+    {
+      if(IRArray[i] == pin)
+      {
+        //newSection = i % 2 == 0 ? (Section)(i / 2) : (Section)((i+1) /2);
+        if(destination == Wilton) 
+        {
+            newSection = (Section)((i / 2) + 1);
+        }
+        else if(destination == Salisbury)
+        {
+            newSection = (Section)(((i+1) / 2) - 1);
+        }
+      }
+    }
+  };
+
+
+};
+
+// Map Data for the track (IRSensors), order Wilton -> Salisbury
+MapData Map[4] = 
+{
+  MapData(IRL1, Wilton),
+  MapData(IRL2, Salisbury),
+  MapData(IRR1, Wilton),
+  MapData(IRR2, Salisbury),
+};
+
+//Function to keep code clean
+void setNextSection(Section &section)
+{
+  /*
+      Iterate over the IRArray to find which sensor is currently active
+      then store that pin in the tempValue variable
+  */
+  for(byte &pin : IRArray)
+  {
+    if(digitalRead(pin) == 0)
+    {
+      tempValue = pin;
+    }
+  }
+
+  /*
+      Iterate over the map data and compare the tempValue to the
+      map data, if the tempValue is the same as the pin for the 
+      map data then set the current section equal to the calculated
+      new section from the map data
+  */
+  for(MapData &data : Map)
+  {
+    if(tempValue == data.pin)
+    {
+      section = data.newSection;
+    }
+  }
+}
