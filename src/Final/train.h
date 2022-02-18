ba197c8ff82c0a66166bb72946d9b3c58f34c55e
Aaron Reason <aaronreason@hotmail.co.uk>
2022-02-18 09:07:34 +0000

    starting re code

--- src/Final/train.h
+++ src/Final/train.h
@@ -11,113 +11,171 @@ byte speedPin = 11;
 // if going to wilton, go forwards, else go back
 
 
+// class Train
+// {
+// public:
+//     Section m_section;
+//     Destination m_destination;
+//     Section m_destinationSection;
+//     Direction m_direction;
+//     byte m_ID;
+//     byte currentSpeed = 0;
+
+//     Train(Section section, Destination destination, byte ID):
+//     m_section(section), m_destination(destination), m_ID(ID)
+//     {
+//         m_destinationSection = destinationSection(m_destination);
+//         m_direction = m_destination == Wilton ? Forward : Backward;
+//     }
+
+//     void update()
+//     {
+
+//         //stop if the next section is not free / red light
+//         if(!nextSectionIsFree())
+//         {
+//             analogWrite(speedPin, 0) ;
+//             return;
+//         } 
+
+
+//         if(m_section != m_destinationSection)
+//         {
+//             // 0 to 255
+//             analogWrite(speedPin, 255);
+//             setMotorPins();
+//             setNextSection();
+//         }
+//         else
+//         {
+//             //currentSpeed -= 1;
+//             analogWrite(speedPin, 0);
+//         }
+//     }
+
+// private:
+
+//     /*
+//         Checks if the next section is free.
+//         If it isn't returns false to tell the train
+//         to stop.
+//     */
+//     bool nextSectionIsFree()
+//     {
+//         for(Signal &signal : SignalArray)
+//         {
+//             if(signal.checkIfIsNextSection(m_section) && signal.m_isEmpty)
+//             {
+//                 return true;
+//             }
+//         }
+
+//         return false;
+//     }
+
+//     /*
+//         Function to update the current section.
+//         provided the sensor is active and is the 
+//         correct sensor
+        
+//         Call this function after checking the next 
+//         section is free
+//     */
+//     void setNextSection()
+//     {
+//         for(MapData &data : Map)
+//         {
+//             if(data.sensorTriggered && (int)data.nextSection == (int)m_section+1)
+//             {
+//                 m_section = data.nextSection;
+//             }
+//         }
+//     }
+
+//     /*
+//         Function to set the direction of the motor
+//         using the direction
+//         anything else means stop
+//     */
+//     void setMotorPins()
+//     {
+//         if(m_direction == Forward)
+//         {
+//             digitalWrite(motor1, HIGH);
+//             digitalWrite(motor2, LOW);
+//         }
+//         else if(m_direction == Backward)
+//         {
+//             digitalWrite(motor1, LOW);
+//             digitalWrite(motor2, HIGH);
+//         }
+//         else
+//         {
+//             digitalWrite(motor1, LOW);
+//             digitalWrite(motor2, LOW);
+//         }
+//     }
+    
+
+
+// };
+
 class Train
 {
 public:
-    Section m_section;
-    Destination m_destination;
-    Section m_destinationSection;
-    Direction m_direction;
-    byte m_ID;
-    byte currentSpeed = 0;
-
-    Train(Section section, Destination destination, byte ID):
-    m_section(section), m_destination(destination), m_ID(ID)
+    
+    Train(byte motorL, byte motorR, byte speedPin,
+    Direction direction, float acceleration, float decleleration):
+    m_motorL(motorL), m_motorR(motorR), m_speedPin(speedPin),
+    m_direction(direction), m_accelerationSpeed(acceleration), m_decelerationSpeed(decleleration)
     {
-        m_destinationSection = destinationSection(m_destination);
-        m_direction = m_destination == Wilton ? Forward : Backward;
+        m_speed = 0;
+        m_maxSpeed = 255;
+        m_shouldDecelerate = false;
+    }
+
+    void start()
+    {
+        digitalWrite(m_motorL, HIGH);
+        digitalWrite(m_motorR, LOW);
+    };
+
+    void stop()
+    {
+        //m_speed = abs(m_speed) >= 0 ? m_speed + 1 * m_deceleration : m_speed;
+        digitalWrite(m_motorL, HIGH);
+        digitalWrite(m_motorR, LOW);
+        m_shouldDecelerate = true;
+    }
+
+    void reverse()
+    {
+        digitalWrite(m_motorL, LOW);
+        digitalWrite(m_motorR, HIGH);
     }
 
     void update()
     {
 
-        //stop if the next section is not free / red light
-        if(!nextSectionIsFree())
+        if(m_shouldDecelerate)
         {
-            analogWrite(speedPin, 0) ;
-            return;
-        } 
+            m_speed =  m_speed >= 0 ? m_speed - 1 * m_decelerationSpeed : m_speed;
+            analogWrite(m_speedPin, (int)m_speed);
 
-
-        if(m_section != m_destinationSection)
-        {
-            // 0 to 255
-            analogWrite(speedPin, 255);
-            setMotorPins();
-            setNextSection();
         }
         else
         {
-            //currentSpeed -= 1;
-            analogWrite(speedPin, 0);
-        }
-    }
+            m_speed = m_speed <= m_maxSpeed ? m_speed + 1 * m_accelerationSpeed : m_speed;
+            analogWrite(m_speedPin, (int)m_speed);
 
-private:
-
-    /*
-        Checks if the next section is free.
-        If it isn't returns false to tell the train
-        to stop.
-    */
-    bool nextSectionIsFree()
-    {
-        for(Signal &signal : SignalArray)
-        {
-            if(signal.checkIfIsNextSection(m_section) && signal.m_isEmpty)
-            {
-                return true;
-            }
         }
 
-        return false;
-    }
-
-    /*
-        Function to update the current section.
-        provided the sensor is active and is the 
-        correct sensor
-        
-        Call this function after checking the next 
-        section is free
-    */
-    void setNextSection()
-    {
-        for(MapData &data : Map)
-        {
-            if(data.sensorTriggered && (int)data.nextSection == (int)m_section+1)
-            {
-                m_section = data.nextSection;
-            }
-        }
-    }
-
-    /*
-        Function to set the direction of the motor
-        using the direction
-        anything else means stop
-    */
-    void setMotorPins()
-    {
-        if(m_direction == Forward)
-        {
-            digitalWrite(motor1, HIGH);
-            digitalWrite(motor2, LOW);
-        }
-        else if(m_direction == Backward)
-        {
-            digitalWrite(motor1, LOW);
-            digitalWrite(motor2, HIGH);
-        }
-        else
-        {
-            digitalWrite(motor1, LOW);
-            digitalWrite(motor2, LOW);
-        }
     }
     
-
-
+private:
+    float m_speed, m_accelerationSpeed, m_decelerationSpeed;
+    byte m_motorL, m_motorR, m_speedPin;
+    Direction m_direction;
+    int m_maxSpeed;
+    bool m_shouldDecelerate;
 };
-
-
