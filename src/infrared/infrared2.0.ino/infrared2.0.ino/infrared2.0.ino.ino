2bc66deef079aab1d5ad3128da3d0cddbe6b70e8
bmajor65 <benmajor6565@yahoo.com>
2022-03-10 11:37:32 +0000

    .

--- src/infrared/infrared2.0.ino/infrared2.0.ino/infrared2.0.ino.ino
+++ src/infrared/infrared2.0.ino/infrared2.0.ino/infrared2.0.ino.ino
@@ -1,52 +1,37 @@
-  int IRR1 = 1;
-  int IRL1 = 2;
-  int GL = 5;
-  int RL = 6;
-
-void setup() {
-  pinMode(IRR1, INPUT);
-  pinMode(IRL1, INPUT);
-  pinMode(GL, OUTPUT);
-  pinMode(RL, OUTPUT);
-}
-
-void loop() {
   class Sensor {
     public:
-      byte IRR1;
-      byte IRL2;
-  };
-  
-  
-  class sensorGroup {
-    byte IRArray[2] = {IRR1, IRL1};
-  };
-  
-  class mapData {
-    byte pin;
-    byte Direction;
-    byte nextSection;
-    bool sensorTriggered;
-  }; 
+      bool state;
+      int pin;
 
+      Sensor(int pin) {
+        this->pin = pin;
+        this->state = false;
+        pinMode(this->pin, INPUT);
+      }
+      void readState() {
+        this->state = digitalRead(this->pin);
+      } 
+  };
+  enum Direction{
+    Forward = 1, 
+    Backward = -1
+  }
+  class SensorGroup {
+    public:
+    Direction
+    SensorGroup(Sensor s1, Sensor s2){
+      this->sensors[0] = s1;
+      this->sensors[1] = s2;
+    }
+    
+    private:
+    Sensor sensors[2];
+  };
+  
+void setup() {
+ 
 }
 
-  int sensorTriggered1 = digitalRead (IRR1);
-  int sensorTriggered2 = digitalRead (IRR1);
 
-  if (sensorTriggered1 == true) then{
-    if (sensorTriggered2 == true)
-    digitalWrite(GL, HIGH);
-//    this shows as going forward
-  };
-  
-  if (sensorTriggered2 == true) then{
-    if (sensorTriggered1 == true)
-    digitalWrite(RL, HIGH);
-//    this shows train going backward
-
-  };
- };
- 
-
-  
+ void loop(){
+ }
