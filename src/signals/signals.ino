fe46dca196d0f3c5057fc8647c3003ac78ec0a60
Benson Jack <bensonjack09@icloud.com>
2022-03-11 10:27:25 +0000

    signal class complete

--- src/signals/signals.ino
+++ src/signals/signals.ino
@@ -1,9 +1,56 @@
-void setup() {
+enum Direction
+{
+  Forward,
+  Backward
+};
+
+class Signal 
+{
+  public:
+    int redPin;
+    int greenPin; 
+    bool state; // 1 = green 0 = red
+    Direction signalDirection;
+  
+  Signal(int redPin, int greenPin, Direction signalDirection) 
+  {
+    this->redPin = redPin;
+    this->greenPin = greenPin;
+    this->signalDirection = signalDirection;
+    state = 1;
+    digitalWrite(greenPin, HIGH)
+  }
+  
+  void changeState()
+  {
+    state = 1 - state
+
+    if (state == 1) 
+    {
+      digitalWrite(redPin, HIGH)
+      digitalWrite(greenPin, LOW) 
+    } else if (state == 0)
+    {
+      digitalWrite(redPin, HIGH)
+      digitalWrite(greenPin, LOW)
+    }
+  }
+
+  bool getState(state)
+  {
+    return state
+  }
+  
+};
+
+void setup() 
+{
   // put your setup code here, to run once:
 
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
 
 }
