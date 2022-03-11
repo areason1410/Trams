dfb6f19ef5be74ba5421de30e162b4863224941c
HSanderson <hay.saw13@gmx.co.uk>
2022-03-11 11:18:37 +0000

    train-class

--- src/trainCode/trainCode.ino
+++ src/trainCode/trainCode.ino
@@ -7,3 +7,20 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+class Train {
+  int currentSpeed;
+  int maxSpeed;
+  int leftMotorPin;
+  int rightMotorPin;
+  int speedPin;
+  enum Direction;
+  bool shouldDecelerate;
+  bool shouldaccelerate;
+
+  Train(int leftMotorPin, int rightMotorPin, int speedPin) {
+    leftMotorPin = 
+    rightMotorPin = 
+    speedPin =    
+  }
+}
