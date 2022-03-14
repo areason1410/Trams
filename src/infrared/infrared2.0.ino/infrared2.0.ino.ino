7e456579fcc4b53457cd5a8e0f17ce2db0b7ed5a
bmajor65 <benmajor6565@yahoo.com>
2022-03-14 13:13:09 +0000

    removed sensorgroup and direction anded an int for siganls

--- src/infrared/infrared2.0.ino/infrared2.0.ino.ino
+++ src/infrared/infrared2.0.ino/infrared2.0.ino.ino
@@ -2,31 +2,35 @@
     public:
       bool state;
       int pin;
+      Signal theSignal;
 
-      Sensor(int pin) {
+      Sensor(int pin, Signal theSignal) {
+
         this->pin = pin;
         this->state = false;
+        this->theSignal = theSignal
         pinMode(this->pin, INPUT);
       }
       void readState() {
         this->state = digitalRead(this->pin);
+        return this->state;
       } 
   };
   enum Direction{
     Forward = 1, 
     Backward = -1
   }
-  class SensorGroup {
-    public:
-    Direction
-    SensorGroup(Sensor s1, Sensor s2){
-      this->sensors[0] = s1;
-      this->sensors[1] = s2;
-    }
+  // class SensorGroup {
+  //   public:
+  //   Direction
+  //   SensorGroup(Sensor s1, Sensor s2){
+  //     this->sensors[0] = s1;
+  //     this->sensors[1] = s2;
+  //   }
     
-    private:
-    Sensor sensors[2];
-  };
+  //   private:
+  //   Sensor sensors[2];
+  // };
   
 void setup() {
  
@@ -35,3 +39,4 @@ void setup() {
 
  void loop(){
  }
+
