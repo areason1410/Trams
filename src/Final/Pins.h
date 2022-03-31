f98b1b2869293c99968814ad095b90d9ee74a8ef
Aaron Reason <aaronreason@hotmail.co.uk>
2022-03-31 16:29:17 +0100

    IR Pins setup

--- src/Final/Pins.h
+++ src/Final/Pins.h
@@ -1,12 +1,12 @@
 #pragma once
 const byte 
-    SalisburyIRRight = 1,
-    BemertonIRLeft = 2,
-    BemertonIRRight = 3,
-    WiltonIRLeft = 4,
+    SalisburyIRRight = 11,
+    BemertonIRLeft = 3,
+    BemertonIRRight = 2,
+    WiltonIRLeft = 8,
 
-    AIRLeft = 5,
-    AIRRight = 6,
+    AIRLeft = 10,
+    AIRRight = 9,
     ALEDRedForward = 22,
     ALEDGreenForward = 23,
     ALEDRedBackward = 24,
@@ -14,15 +14,15 @@ const byte
 
 
 
-    BIRLeft = 7,
-    BIRRight = 8,
+    BIRLeft = 5,
+    BIRRight = 4,
     BLEDRedForward = 42,
     BLEDGreenForward = 43,
     BLEDRedBackward = 44,
     BLEDGreenBackward = 45,
 
-    CIRLeft = 9,
-    CIRRight = 10,
+    CIRLeft = 6,
+    CIRRight = 7,
     CLEDRedForward = 52,
     CLEDGreenForward = 53,
     CLEDRedBackward = 54,
